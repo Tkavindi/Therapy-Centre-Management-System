3aa18719bfa36a1308c43c2d79b6508bd7b5f576
Tkavindi <tharushak77@gmail.com>
2025-03-29 20:09:49 +0530

    changed

--- back-end/Models/BookingModels.js
+++ back-end/Models/BookingModels.js
@@ -1,36 +1,20 @@
-// const mongoose = require('mongoose')
-
-// const bookingSchema = new mongoose.Schema({
-//     customer_name: { type: String, required: true},
-//     customer_country: { type: String, required: true},
-//     massage_type: { type: String, required: true},
-//     duration: { type: Number, required: true},
-//     date: {type: Date, required: true},
-//     time: {type: String, required: true},
-//     therapist_name: {type: String, required: false},
-//     therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true},
-//     price: {type: Number, required: true },
-//     status: {type: String, default: "Pending"}
-
-    
-// })
-
-
-// module.exports =  mongoose.model('Booking', bookingSchema)
-
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const bookingSchema = new mongoose.Schema({
-    customer_name: { type: String, required: true },
-    customer_country: { type: String, required: true }, // Updated field
-    massage_type: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, required: true },
-    time: { type: String, required: true },
-    therapist_name: { type: String, required: false },
-    therapist_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
-    price: { type: Number, required: true },
-    status: { type: String, default: "Pending" }
-});
+    customer_name: { type: String, required: true},
+    customer_country: { type: String, required: true},
+    massage_type: { type: String, required: true},
+    duration: { type: Number, required: true},
+    date: {type: Date, required: true},
+    time: {type: String, required: true},
+    therapist_name: {type: String, required: false},
+    therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true},
+    price: {type: Number, required: true },
+    status: {type: String, default: "Pending"}
 
-module.exports = mongoose.model('Booking', bookingSchema);
+    
+})
+
+
+module.exports =  mongoose.model('Booking', bookingSchema)
+
