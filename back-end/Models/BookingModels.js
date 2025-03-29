e7f5e8244c8c7c33beca6c33604c83f65002e16f
Tkavindi <tharushak77@gmail.com>
2025-03-29 20:00:06 +0530

    Change BookingModel

--- back-end/Models/BookingModels.js
+++ back-end/Models/BookingModels.js
@@ -1,19 +1,36 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const bookingSchema = new mongoose.Schema({
-    customer_name: { type: String, required: true},
-    customer_country: { type: String, required: true},
-    massage_type: { type: String, required: true},
-    duration: { type: Number, required: true},
-    date: {type: Date, required: true},
-    time: {type: String, required: true},
-    therapist_name: {type: String, required: false},
-    therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true},
-    price: {type: Number, required: true },
-    status: {type: String, default: "Pending"}
+// const bookingSchema = new mongoose.Schema({
+//     customer_name: { type: String, required: true},
+//     customer_country: { type: String, required: true},
+//     massage_type: { type: String, required: true},
+//     duration: { type: Number, required: true},
+//     date: {type: Date, required: true},
+//     time: {type: String, required: true},
+//     therapist_name: {type: String, required: false},
+//     therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true},
+//     price: {type: Number, required: true },
+//     status: {type: String, default: "Pending"}
 
     
-})
+// })
 
 
-module.exports =  mongoose.model('Booking', bookingSchema)
+// module.exports =  mongoose.model('Booking', bookingSchema)
+
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    customer_name: { type: String, required: true },
+    customer_country: { type: String, required: true }, // Updated field
+    massage_type: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    therapist_name: { type: String, required: false },
+    therapist_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+    price: { type: Number, required: true },
+    status: { type: String, default: "Pending" }
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
